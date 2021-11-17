b407efc3954095786b46587efb4250ac93f8190e
JordiNodeJS <jordi.nodejs@gmail.com>
2021-11-17 01:23:35 +0100

    v1.2

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let indexImgs = 0
+let index = 0
 const imgs = document.querySelectorAll('img')
 const slideShow = () => {
   imgs.forEach(img => (img.className = 'no-visible'))
-  imgs[indexImgs].className = 'visible'
-  indexImgs++
-  if (indexImgs >= imgs.length) indexImgs = 0
+  imgs[index].className = 'visible'
+  index++
+  if (index >= imgs.length) index = 0
   setTimeout(slideShow, 2000)
 }
 slideShow()
